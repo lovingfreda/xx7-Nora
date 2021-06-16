24aba613b157864ed4ca3aad4628c48d52f3039a
Tony <tony@kideduc.com>
2021-06-16 09:50:06 +0800

    support service detail popup
    adding a new page of  CruiseAjaxLinkSumry

--- src/js/routes.js
+++ src/js/routes.js
@@ -14,6 +14,7 @@ import LeftPage1 from '../pages/left-page-1.f7.html';
 import LeftPage2 from '../pages/left-page-2.f7.html';
 
 import CruiseAjaxLoad from '../pages/cruise-ajax-load.f7.html';
+import CruiseAjaxLinkSumry from '../pages/cruise-ajax-link-summry.f7.html';
 
 var routes = [
   {
@@ -117,15 +118,15 @@ var routes = [
         // We got user data from request
         var vCatg = {
           firstName: 'Dear',
-          lastName: 'My Lover',
-          about: 'Hello, i am FUCKING YangY of Framework7! Hope you like it!',
+          lastName: 'My Passengers',
+          about: ' 我们为您提供以下服务，希望您喜欢!',
           links: [
             {
-              title: 'Fucking YangY Website',
+              title: 'Fall in lover with YangY Website',
               url: 'http://framework7.io',
             },
             {
-              title: 'Fucking Eva Forum',
+              title: 'Loving Eva Forum',
               url: 'http://forum.framework7.io',
             },
           ]
@@ -158,6 +159,75 @@ var routes = [
         resolve(
           {
             component: CruiseAjaxLoad,
+          },
+          {
+            props: {
+              catg: catg,
+            }
+          }
+        );
+*/
+      }, 1000);
+    },
+  },
+  {
+    path: '/request-and-load/links/:catgId/',
+    async: function ({ router, to, resolve }) {
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var cateId = to.params.catgId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var vCatg = {
+          firstName: 'Dear',
+          lastName: 'My Passengers',
+          about: ' 我们为您提供以下服务，希望您喜欢!',
+          links: [
+            {
+              title: 'Fall in lover with YangY Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Loving Eva Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Tony Adding Main Logic of AJAX :  1. Larabel 2. CCS Studio
+//      app.request.get('http://localhost:8083/products', function(data) {
+        app.request.get('http://localhost:8086/services/links.php?node='+cateId, function(data) {
+              // Hide Preloader
+              app.preloader.hide();
+
+              // Resolve route to load page
+              resolve(
+                  {
+                      component: CruiseAjaxLinkSumry,
+                  },
+                  {
+                      props: {
+                          catg: vCatg,
+                          prdt: data,
+                      }
+                  }
+              );
+
+          });
+/*
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            component: CruiseAjaxLinkSumry,
           },
           {
             props: {
